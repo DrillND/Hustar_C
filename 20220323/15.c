c9c1038e2fafeea9146625638500f22793c5e6a3
DrillND <gornhub13@gmail.com>
2022-03-23 17:54:56 +0900

    1

--- 20220323/15.c
+++ 20220323/15.c
@@ -1,40 +1,29 @@
+// ppt 3 p.22
 #include <stdio.h>
-//ppt 3 p.22
+
+#define M 3 /* number of rows */
+#define N 4 /* number of columns */
 
 int main()
 {
-    int a[3][4], i, j;
-    int sum[4];
-    sum[0] = 0;
+    int a[M][N];
     int num;
-
-    for (int i = 0; i < 12; i++) //12개의 숫자 입력 받기
-
-
-    
-    {
-        printf("input integer %d  : ", i + 1);
-        scanf("%d", &num);
-        
-        
-    }
-
-    for (i = 0; i < 3; ++i)
-    {
-
-        for (j = 0; j < 4; ++j)
+    /*
+        for (int i = 0; i < M * N; i++) // 12개의 숫자 입력 받기
         {
-            a[i][j] = i + j;
+            printf("input integer %d  : ", i + 1);
+            scanf("%d", &num);
         }
-        sum[i] = sum[i] + a[i][j];
-    }
-
-    for (i = 0; i < 3; ++i)
+    */
+    for (int i = 0; i < M; i++)
     {
-        for (j = 0; j < 4; ++j)
-            printf("a[%d][%d]=%d ", i, j, a[i][j]);
+        for (int j = 0; j < N; j++)
+        {
+            printf("input integer : ");
+            scanf("%d", &num);
+            a[i][j]=num;
+            printf("%5d", a[i][j]);
+        }
         printf("\n");
     }
-
-    printf("\nsum = % d\n ", sum);
 }
